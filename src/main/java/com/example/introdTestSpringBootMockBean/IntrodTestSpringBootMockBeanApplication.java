900224824b107e6c925fdf448ac3b12c6112ac02
WelBert-dev <wellison.bertelli@hotmail.com>
2022-09-17 18:46:52 -0300

    First Commit

---
+++ src/main/java/com/example/introdTestSpringBootMockBean/IntrodTestSpringBootMockBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.introdTestSpringBootMockBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrodTestSpringBootMockBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntrodTestSpringBootMockBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/introdTestSpringBootMockBean/IntrodTestSpringBootMockBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introdTestSpringBootMockBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrodTestSpringBootMockBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
